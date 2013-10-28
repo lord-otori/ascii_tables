e8bb407a51906da9fb579830d8e68d7ece1ce05e
Juan S. Simon <juan.simon@gmail.com>
2013-10-28 20:57:08 -0300

    Changed Class name from ascii_tables to AsciiTables

--- lib/ascii_tables.dart
+++ lib/ascii_tables.dart
@@ -12,42 +12,42 @@ const PAD_LEFT = 2;
 const PAD_BOTH = 3;
 
 
-class ascii_tables{
-  
-  
+class AsciiTables{
+
+
   bool _isPrintHeaderEnabled = true;
   String _tableHeaderString;
   String _tableBodyString;
   int _padding = 1;
   int _from_type;
   Map <String, int> _column_sizes;
-  
-  
+
+
   Map <String, Map <String, String>> _content_map = new Map();
 
-  
-  
+
+
   HeaderBuilder _hb = new HeaderBuilder();
   BodyBuilder _bb = new BodyBuilder();
   TableMeasure _tm = new TableMeasure();
-  
-  ascii_tables.fromMap(Map <String, Map <String, String>> map){
+
+  AsciiTables.fromMap(Map <String, Map <String, String>> map){
     this._content_map = map;
     this._column_sizes = this._tm.fromMap(map);
   }
   
-  ascii_tables.fromList(List <Map <String, String>> list) {
+  AsciiTables.fromList(List <Map <String, String>> list) {
     this._content_map = new Map.fromIterable(list);
     this._column_sizes = this._tm.fromMap(this._content_map);
   }
   
-  ascii_tables.fromSet(Set set) {
+  AsciiTables.fromSet(Set set) {
 
   }
 
-  ascii_tables.fromIterator(Iterator <String> i){
+  AsciiTables.fromIterator(Iterator <String> i){
 
-  }  
+  }
   
   void displayHeader(bool display_header) {
     this._isPrintHeaderEnabled = display_header;
@@ -73,21 +73,21 @@ class ascii_tables{
       this._bb.setColumnSizes(this._column_sizes);
       this._tableHeaderString = this._hb.fromMap(this._content_map);
       this._tableBodyString = this._bb.fromMap(this._content_map);
-    
-    
+
+
     FormatString fs = new FormatString();
     
     int total_length = this._column_sizes.length * 2 * this._padding;
     this._column_sizes.forEach((k,v) {
       total_length += v;
     });
-    total_length += (this._column_sizes.length -1);    
-    table += '+' + fs.str_repeat('-', total_length) + '+\n';    
+    total_length += (this._column_sizes.length -1);
+    table += '+' + fs.str_repeat('-', total_length) + '+\n';
     if(this._isPrintHeaderEnabled){
       table += this._tableHeaderString + '\n';
     }
       table += this._tableBodyString + '\n';
       table += '+' + fs.str_repeat('-', total_length) + '+';
       return table;
-  }  
+  }
 }
